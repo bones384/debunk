106537af593acce17dd1af8a63773b0850b1bb57
bones384 <mkowalec78@gmail.com>
2026-04-22 12:36:07 +0200

    Fixed CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,14 +59,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "backend.urls"
@@ -139,3 +138,4 @@ STATIC_URL = "static/"
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = os.environ.get("CORS_ALLOWED_ORIGINS","http://localhost:5173").split(",")
